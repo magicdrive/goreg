e376ec26cb1f009706fe41400153d9da8d1f9fbf
Hiroshi IKEGAMI <hiroshi.ikegami@magicdrive.jp>
2026-04-08 08:30:38 +0900

    Added completion init cmd.

--- misc/completions/bash/goreg-completion.bash
+++ misc/completions/bash/goreg-completion.bash
@@ -1,12 +1,23 @@
 #!/bin/bash
 
 _goreg_completions() {
-    local cur prev opts
+    local cur prev opts subcommands
     COMPREPLY=()
     cur="${COMP_WORDS[COMP_CWORD]}"
     prev="${COMP_WORDS[COMP_CWORD-1]}"
 
     opts="-h --help -v --version -w --write -l --local -o --order -n --organization -m --minimize-group -a --sort-include-alias -r --remove-import-comment"
+    subcommands="init"
+
+    # If we're at the first argument position, suggest subcommands and options
+    if [[ ${COMP_CWORD} -eq 1 ]]; then
+        if [[ ${cur} == -* ]]; then
+            COMPREPLY=( $(compgen -W "${opts}" -- ${cur}) )
+        else
+            COMPREPLY=( $(compgen -W "${subcommands}" -- ${cur}) $(compgen -f -X '!*.go' -- "$cur") )
+        fi
+        return 0
+    fi
 
     # Suggest options
     if [[ ${cur} == -* ]]; then
